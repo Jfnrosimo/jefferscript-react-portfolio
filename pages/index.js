1afc4c0c109399bd402a697a749974e9a8fa14b5
jrosi <j.rosimo098@gmail.com>
2022-12-12 22:08:03 +0800

    Fix: image paths

--- pages/index.js
+++ pages/index.js
@@ -17,13 +17,13 @@ import { TbBrandJavascript, TbBrandNextjs } from "react-icons/tb";
 
 //Import images
 import Image from "next/image";
-import devIcon from "../public/programmer.png";
-import portfolio from "../public/portfolio-p1.png";
-import slytheryn from "../public/slytheryn-p2.png";
-import vepply from "../public/vepply-p3.png";
-import barangayCencus from "../public/project5-sc.png";
-import ems from "../public/ems-p6.png";
-import kondisyon from "../public/hackathon-project-sc.png";
+import devIcon from "/programmer.png";
+import portfolio from "/portfolio-p1.png";
+import slytheryn from "/slytheryn-p2.png";
+import vepply from "/vepply-p3.png";
+import barangayCencus from "/project5-sc.png";
+import ems from "/ems-p6.png";
+import kondisyon from "/hackathon-project-sc.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
